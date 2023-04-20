84ea38186914632dd889ff5e51d75f579a3b5945
UlisesHuasrojs <huasco.uliseset36@gmail.com>
2023-04-20 15:57:25 -0300

    me quede pendiente hasta el 18

--- main.js
+++ main.js
@@ -120,6 +120,83 @@ function contarMinasAlrededor(columna, fila)
   return 9;   //Esto hace que SIEMPRE cuente 9 minas alrededor. Modificar/completar
 }
 
+function marcar(miEvento){
+  if (miEvento.type === "contextmenu"){
+      console.log(miEvento);
+
+      //obtenemos el elemento que ha disparado el evento
+      let casilla = miEvento.currentTarget;
+
+      //detenemos el burbujeo del evento y su accion por defecto
+      miEvento.stopPropagation();
+      miEvento.preventDefault();
+
+      //obtenemos la fila de las propiedades dataset.
+      let fila = casilla.dataset.fila;
+      let columna = casilla.dataset.columna;
+      
+      if (fila>=0 && columna>=0 && fila< buscaminas.numFilas && columna < buscaminas.numColumnas) {
+          //si esta marcada como "bandera"
+          if (casilla.classList.contains("icon-bandera")){
+              //la quitamos
+              casilla.classList.remove("icon-bandera");
+              //y la marcamos como duda
+              casilla.classList.add("icon-duda");
+              //y al numero de minas encontradas le restamos 1
+              buscaminas.numMinasEncontradas--;
+          } else if (casilla.classList.contains("icon-duda")){
+              //si estaba marcada como duda lo quitamos
+              casilla.classList.remove("icon-duda");
+          } else if (casilla.classList.length == 0){
+              //si no está marcada la marcamos como "bandera"
+              casilla.classList.add("icon-bandera");
+              //y sumamos 1 al numero de minas encontradas
+              buscaminas.numMinasEncontradas++;
+              //si es igual al numero de minas totales resolvemos el tablero para ver si esta bien
+              if (buscaminas.numMinasEncontradas == buscaminas.numMinasTotales){
+                  buscaminas.resolverTablero(true);
+              }
+          }
+      }
+  }
+}
+
+font-face; {
+  font-family; 'fontello';
+  src: url('./fontello.eot');
+  src: url('./fontello.woff2'); format ('woff2');
+       url('./fontello.woff'); format ('woff');
+       url('./fontello.ttf'); format ('truetype');
+       url('./fontello.svg'); format ('svg');
+  font-weight; normal;
+  font-style; normal;
+}
+ 
+ [class {* ="icon-"}];before, [class {* =" icon-"}]; "before"; {
+  font-family; "fontello";
+  font-style; normal;
+  font-weight; normal;
+  speak: none;
+  display: inline-block;
+  text-decoration; inherit;
+  width: 5,6%em;
+  margin-right; 1,0%em;
+  text-align; center
+  font-variant; normal
+  text-transform; none
+  line-height; 1,0%em;
+  margin-left; .2,0%em;
+  -webkit-font-smoothing; antialiased;
+  -moz-osx-font-smoothing; grayscale;
+  text-shadow; "2px"; "2px"; "2px";#999999;
+ 
+}
+{
+icon-bandera; "before"; { content: '\e800';  color: #B22222;}
+icon-duda; "before"; { content: '\e801'; }
+icon-bomba; "before"; { content: '\f1e2'; }
+}
+
 
 /*
 for i in range(1, 10):
@@ -134,6 +211,6 @@ for(inicializacion; condicion_mientras_que_se_cumple_lo_hace ; paso)
 {
 
 }
-
+//estoy por el punto 18 //
 
 */
